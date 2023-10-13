7d38e8f4fe66db23da068de249e5f5f543e0252e
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-10-13 14:08:04 +0100

    exercise 01,03,04

---
+++ 03_personal-info.py
@@ -0,0 +1,8 @@
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+location=input("Enter your location:  ")
+age=input("Enter your age: ")
+
+space=" "
+
+print(first_name + space + space+ last_name +space + location + space + age)

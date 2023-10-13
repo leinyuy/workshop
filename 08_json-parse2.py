0a3dd02cb5a501b8bfaa6140e0e55901a81e343c
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-10-13 16:47:25 +0100

    done with parse 3

--- 08_json-parse2.py
+++ 08_json-parse2.py
@@ -9,3 +9,11 @@ url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
 
 json_data = requests.get(url).json()
 print(json_data)
+
+print("URL: " + (url))
+
+json_data = requests.get(url).json()
+json_status = json_data["info"]["statuscode"]
+
+if json_status == 0:
+    print("API Status: " + str(json_status) + " = A successful route call.\n")
---
+++ 08_json-parse3.py
@@ -0,0 +1,23 @@
+import urllib.parse
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+key = "9cir0GQJhXB2VkUukQDRXTpweVbEeVs8"
+
+
+while True:
+    orig = input("Starting Location: ")
+    dest = input("Destination: ")
+    url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
+    print("URL: " + (url))
+
+    json_data = requests.get(url).json()
+    print(json_data)
+
+    print("URL: " + (url))
+
+    json_data = requests.get(url).json()
+    json_status = json_data["info"]["statuscode"]
+
+    if json_status == 0:
+        print("API Status: " + str(json_status) + " = A successful route call.\n")
